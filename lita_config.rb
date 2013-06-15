f8d9589e0e32fdf20d58e39e3cfca5bb80a3d471
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-15 03:07:18 -0700

    Ignore lita_config.rb

--- lita_config.rb
+++
@@ -1,6 +0,0 @@
-Lita.configure do |config|
-  config.adapter.name = :irc
-  config.adapter.server = "irc.freenode.net"
-  config.adapter.channels = ["#litabot"]
-  config.adapter.nick = "LitaBot"
-end
